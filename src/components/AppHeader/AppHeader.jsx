e8b6a5812cb7ef7efc469055e131ea374f4a78da
M0riaz <118534241+M0riaz@users.noreply.github.com>
2023-05-16 19:33:26 +0300

    исправления после первого ревью

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -9,17 +9,25 @@ export default function AppHeader() {
                 <nav className={styles.header__navigation}>
                     <div className={styles.header__item}>
                         <BurgerIcon type={"primary"}/>
-                        <p className="text text_type_main-default mr-5">Конструктор</p>
+                        <p className='text text_type_main-default ml-3'>
+                            <a href='#' className={`${styles.link} mr-5`}>Конструктор</a>
+                        </p>
+
                     </div>
                     <div className={`${styles.header__item} ml-2`}>
                         <ListIcon type={"secondary"}/>
-                        <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
+                        <p className='text text_type_main-default text_color_inactive ml-3'>
+                            <a href='#' className={`${styles.linkInactive} mr-5`}>Лента заказов</a>
+                        </p>
+
                     </div>
                 </nav>
                 <Logo/>
                 <div className={`${styles.header__item} `}>
                     <ProfileIcon type={"secondary"}/>
-                    <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
+                    <p className="text text_type_main-default text_color_inactive ml-2">
+                        <a href='#' className={`${styles.linkInactive} mr-5`}>Личный кабинет</a>
+                    </p>
                 </div>
             </div>
         </header>
